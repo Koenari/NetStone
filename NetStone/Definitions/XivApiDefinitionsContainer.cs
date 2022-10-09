979d24bef52fb25f7226d467a09940ba2a19189b
ge7nic <48736317+ge7nic@users.noreply.github.com>
2022-10-09 20:12:33 +0200

    Remove Patch-Selector Method for materia

--- NetStone/Definitions/XivApiDefinitionsContainer.cs
+++ NetStone/Definitions/XivApiDefinitionsContainer.cs
@@ -30,7 +30,6 @@ public class XivApiDefinitionsContainer : DefinitionsContainer
         this.Character = await GetDefinition<CharacterDefinition>("profile/character.json");
         this.ClassJob = await GetDefinition<CharacterClassJobDefinition>("profile/classjob.json");
         this.Gear = await GetDefinition<CharacterGearDefinition>("profile/gearset.json");
-        PatchCharacterGearDefinition();
 
         this.Attributes = await GetDefinition<CharacterAttributesDefinition>("profile/attributes.json");
         this.Achievement = await GetDefinition<PagedDefinition>("profile/achievements.json");
@@ -51,36 +50,6 @@ public class XivApiDefinitionsContainer : DefinitionsContainer
     {
         var json = await this.client.GetStringAsync(path);
         return JsonConvert.DeserializeObject<T>(json);
-    }
-
-    // Remove every character in materia selector after <ul> to make a search after css classes for materia possible.
-    // This is needed, because ``ul:nth-child(7)`` doesn't work if the item has been dyed.
-    private void PatchCharacterGearDefinition()
-    {
-        var oldSelector = "ul:nth-child(7)";
-        var newSelector = "ul.db-tooltip__materia";
-
-        PatchAllMateriaSlots(this.Gear.Mainhand);
-        PatchAllMateriaSlots(this.Gear.Offhand);
-        PatchAllMateriaSlots(this.Gear.Head);
-        PatchAllMateriaSlots(this.Gear.Body);
-        PatchAllMateriaSlots(this.Gear.Hands);
-        PatchAllMateriaSlots(this.Gear.Legs);
-        PatchAllMateriaSlots(this.Gear.Feet);
-        PatchAllMateriaSlots(this.Gear.Earrings);
-        PatchAllMateriaSlots(this.Gear.Necklace);
-        PatchAllMateriaSlots(this.Gear.Bracelets);
-        PatchAllMateriaSlots(this.Gear.Ring1);
-        PatchAllMateriaSlots(this.Gear.Ring2);
-
-        void PatchAllMateriaSlots(GearEntryDefinition gearEntryDefinition)
-        {
-            gearEntryDefinition.Materia1.Selector = gearEntryDefinition.Materia1.Selector.Replace(oldSelector, newSelector);
-            gearEntryDefinition.Materia2.Selector = gearEntryDefinition.Materia2.Selector.Replace(oldSelector, newSelector);
-            gearEntryDefinition.Materia3.Selector = gearEntryDefinition.Materia3.Selector.Replace(oldSelector, newSelector);
-            gearEntryDefinition.Materia4.Selector = gearEntryDefinition.Materia4.Selector.Replace(oldSelector, newSelector);
-            gearEntryDefinition.Materia5.Selector = gearEntryDefinition.Materia5.Selector.Replace(oldSelector, newSelector);
-        }
     }
 
     public override void Dispose()
